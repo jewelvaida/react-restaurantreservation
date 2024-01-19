7ef8f817e90602e1d35b9a06dd19d111d0a69347
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-01-19 11:47:38 +0800

    Updated frontend

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,17 +5,18 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">HEAVEN'S KITCHEN</div>
+          {/* <img src="/tablo.png" alt="" /> */}
+          <div className="left"><img src="/tablo.png" alt="" /></div>
           <div className="right">
-            <p>Open: 09:00 AM - 10:00 PM</p>
+            <p className="p1">Open: 09:00 AM - 10:00 PM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By GROUP 7</p>
+            <p className="p2">Developed By GROUP 7</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By GROUP 7.</p>
+            <p>All Rights Reserved By GROUP 7</p>
           </div>
         </div>
       </div>
