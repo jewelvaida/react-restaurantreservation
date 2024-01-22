2f5d436827835ac5fa2e1f500fcf10fb90aac8a8
jewelvaida <jewelvaidaajaime@gmail.com>
2024-01-22 11:04:42 +0800

    Updated footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,12 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div className="banner">  
-          <div className="left"><img src="/tablo.png" alt="" /></div>
+        <div className="banner">
+          <div className="left">
+            <img src="/tablo.png" alt="" />
+          </div>
           <div className="right">
-            <p className="p1">Open: 09:00 AM - 10:00 PM</p>
+            <p className="p1">Open: 10:00 AM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
